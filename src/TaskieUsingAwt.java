8c110b1dd89c4ae6ec5197b4106dc9b7a5e853f5
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-09-05 14:36:21 +0800

    Added test java GUI using awt

---
+++ src/TaskieUsingAwt.java
@@ -0,0 +1,20 @@
+import java.awt.*;   
+
+public class TaskieUI extends Frame {
+	public TaskieUI() {
+		setLayout(new GridLayout(0, 1, 0, 0));
+		add(new Label("Today's task(s):"));
+		add(new Label("Order pizza at 7pm"));
+		add(new Label("Refelection by 11pm"));
+		TextField input = new TextField();
+		add(input);
+		// input.addActionListener(this);
+		
+		setTitle("Taskie");
+		setSize(350, 120);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+        new TaskieUI();
+    }
+}
