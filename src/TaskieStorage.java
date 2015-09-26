06df70373b503f42e513afcad7dbf4fb051a18a7
Qin Xueying <qinxueying1995@gmail.com>
2015-09-27 00:49:25 +0800

    modification
    // that’s all for today… I am thinking store eventdeadline and float
    into two txt file… will be easier for operation..
    anyway will finish it tmr. Good night.

--- src/TaskieStorage.java
+++ src/TaskieStorage.java
@@ -4,14 +4,40 @@
  *
  */
 import java.util.*;
-public class TaskieStorage {
-	private ArrayList<Task> EventDeadlineTaskList;
-	private ArrayList<Task> FloatTaskList;
-	private HashMap<Date, ArrayList<Task>> EventDeadlineStartDateMap;
-	private HashMap<Date, ArrayList<Task>> EventDeadlineEndDateMap;
-	private HashMap<Date, ArrayList<Task>> FloatDateMap;
-	private HashMap<TaskPriority, ArrayList<Task>> EventDeadlinePriorityMap;
-	private HashMap<TaskPriority, ArrayList<Task>> FloatPriorityMap;
-	private Stack<HashMap<String, Object>> commandStack;
+import java.io.*;
 
+public class TaskieStorage {
+	private File taskFile;
+	private ArrayList<Task> eventDeadlineTaskList;
+	private ArrayList<Task> floatTaskList;
+	private HashMap<Date, ArrayList<Task>> eventDeadlineStartDateMap;
+	private HashMap<Date, ArrayList<Task>> eventDeadlineEndDateMap;
+	private HashMap<Date, ArrayList<Task>> floatDateMap;
+	private HashMap<TaskPriority, ArrayList<Task>> eventDeadlinePriorityMap;
+	private HashMap<TaskPriority, ArrayList<Task>> floatPriorityMap;
+	private Stack<HashMap<String, Object>> commandStack;
+		
+	public TaskieStorage(String pathName){
+		taskFile = new File(pathName);
+		eventDeadlineTaskList = new ArrayList<Task>();
+		floatTaskList = new ArrayList<Task>();
+		eventDeadlineStartDateMap = new HashMap<Date, ArrayList<Task>>();
+		eventDeadlineEndDateMap = new HashMap<Date, ArrayList<Task>>();
+		floatDateMap = new HashMap<Date, ArrayList<Task>>();
+		eventDeadlinePriorityMap = new HashMap<TaskPriority, ArrayList<Task>>();
+		floatPriorityMap = new HashMap<TaskPriority, ArrayList<Task>>();
+		commandStack = new Stack<HashMap<String, Object>>();
+	}
+	
+	public void load(){
+		
+	}
+	
+	public ArrayList<Task> displayEventDeadline(){
+		return this.eventDeadlineTaskList;
+	}
+	public ArrayList<Task> displayFloatTask(){
+		return this.floatTaskList;
+	}
+	
 }
