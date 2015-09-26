97e02c63f6642a74558f2f7edf1baea25b5b8fb1
Qin Xueying <qinxueying1995@gmail.com>
2015-09-27 00:11:49 +0800

    define attr.

--- src/TaskieStorage.java
+++ src/TaskieStorage.java
@@ -1,4 +1,17 @@
-
+/**
+ * The Taskie storage component
+ * @author Qin Xueying
+ *
+ */
+import java.util.*;
 public class TaskieStorage {
+	private ArrayList<Task> EventDeadlineTaskList;
+	private ArrayList<Task> FloatTaskList;
+	private HashMap<Date, ArrayList<Task>> EventDeadlineStartDateMap;
+	private HashMap<Date, ArrayList<Task>> EventDeadlineEndDateMap;
+	private HashMap<Date, ArrayList<Task>> FloatDateMap;
+	private HashMap<TaskPriority, ArrayList<Task>> EventDeadlinePriorityMap;
+	private HashMap<TaskPriority, ArrayList<Task>> FloatPriorityMap;
+	private Stack<HashMap<String, Object>> commandStack;
 
 }
