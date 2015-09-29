ede86aa2f56fbf4a8a0b1741fc4dde3ddd1a204a
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-09-30 04:34:48 +0800

    Renamed unit test class for parser

--- src/TaskieParserTest.java
+++ src/TaskieParserTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TaskieTaskTest {
+public class TaskieParserTest {
 
 	@Before
 	public void setUp() throws Exception {
