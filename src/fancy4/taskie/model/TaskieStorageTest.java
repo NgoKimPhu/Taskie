ff7116bb64298f1b5ad482be92f56ae9aa7af5aa
Qin Xueying <qinxueying1995@gmail.com>
2015-10-04 23:11:29 +0800

    initialize test

--- src/fancy4/taskie/model/TaskieStorageTest.java
+++ src/fancy4/taskie/model/TaskieStorageTest.java
@@ -2,8 +2,14 @@ package fancy4.taskie.model;
 
 public class TaskieStorageTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
+		try {
+			TaskieStorage.load("/test");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
