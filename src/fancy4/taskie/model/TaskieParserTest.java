96710fe53e199928424acab571e694829445e364
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-10-09 16:46:12 +0800

    Removed redundant prints

--- src/fancy4/taskie/model/TaskieParserTest.java
+++ src/fancy4/taskie/model/TaskieParserTest.java
@@ -17,26 +17,16 @@ public class TaskieParserTest {
 
 	@Test
 	public void test() {
-		TaskieAction action = TaskieParser.parse("add test monday");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("buy 5 apples");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("add test from today till mon");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("buy pizza 7pm today");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("tmr 2 to 3"); // should I recognize this as time?
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("class from 2 to 3");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("conference 7 today till 9pm tmr");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("conference tmr from 5 to 6 pm"); //TODO
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("conference 7.15pm till 9.30 tmr night at th");
-		System.err.println("^ " + action.getTask().getTitle());
-		action = TaskieParser.parse("conference today 7.15pm till 9.30 tmr night at th");
-		System.err.println("^ " + action.getTask().getTitle());
+		TaskieParser.parse("add test monday");
+		TaskieParser.parse("buy 5 apples");
+		TaskieParser.parse("add test from today till mon");
+		TaskieParser.parse("buy pizza 7pm today");
+		TaskieParser.parse("tmr 2 to 3"); // should I recognize this as time?
+		TaskieParser.parse("class from 2 to 3");
+		TaskieParser.parse("conference 7 today till 9pm tmr");
+		TaskieParser.parse("conference tmr from 5 to 6 pm"); //TODO
+		TaskieParser.parse("conference 7.15pm till 9.30 tmr night at th");
+		TaskieParser.parse("conference today 7.15pm till 9.30 tmr night at th");
 		
 	}
 
