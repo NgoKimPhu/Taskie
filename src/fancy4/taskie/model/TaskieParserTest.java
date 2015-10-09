1446975ed31f628f028c5fd686c746023e3986d9
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-10-09 17:56:12 +0800

    Add more tests for recognizing optional task type parameter

--- src/fancy4/taskie/model/TaskieParserTest.java
+++ src/fancy4/taskie/model/TaskieParserTest.java
@@ -19,14 +19,20 @@ public class TaskieParserTest {
 	public void test() {
 		TaskieParser.parse("add test monday");
 		TaskieParser.parse("buy 5 apples");
+		TaskieParser.parse("buy 5 apples float");
+		TaskieParser.parse("buy 5 apples -float");
 		TaskieParser.parse("add test from today till mon");
+		TaskieParser.parse("reflection 2 due 2359 tmr");
+		TaskieParser.parse("reflection 2 deadline 2359 tmr");
+		TaskieParser.parse("reflection 2 -deadline 2359 tmr");
 		TaskieParser.parse("buy pizza 7pm today");
-		TaskieParser.parse("tmr 2 to 3"); // should I recognize this as time?
+		TaskieParser.parse("lab tmr 2 to 3"); // should I recognize this as time?
 		TaskieParser.parse("class from 2 to 3");
-		TaskieParser.parse("conference 7 today till 9pm tmr");
-		TaskieParser.parse("conference tmr from 5 to 6 pm");
-		TaskieParser.parse("conference 7.15pm till 9.30 tmr night at th");
-		TaskieParser.parse("conference today 7.15pm till 9.30 tmr night at th");
+		TaskieParser.parse("conference 7 today till 9pm tomorrow");
+		TaskieParser.parse("conference event 7 today till 9pm tmr");
+		TaskieParser.parse("+ dinner with myself tmr from 5 to 6 pm");
+		TaskieParser.parse("dance 7.15pm till 9.30 tmr night at th");
+		TaskieParser.parse("party today 7.15pm till 9.30 tmr night at th");
 		
 	}
 
