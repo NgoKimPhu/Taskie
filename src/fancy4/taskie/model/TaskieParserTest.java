412e97056a30f09508e77e27623d036894f11461
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-10-09 18:19:42 +0800

    Edit some test cases

--- src/fancy4/taskie/model/TaskieParserTest.java
+++ src/fancy4/taskie/model/TaskieParserTest.java
@@ -19,14 +19,14 @@ public class TaskieParserTest {
 	public void test() {
 		TaskieParser.parse("add test monday");
 		TaskieParser.parse("buy 5 apples");
-		TaskieParser.parse("buy 5 apples float");
+		TaskieParser.parse("i want to float");
 		TaskieParser.parse("buy 5 apples -float");
 		TaskieParser.parse("add test from today till mon");
-		TaskieParser.parse("reflection 2 due 2359 tmr");
-		TaskieParser.parse("reflection 2 deadline 2359 tmr");
-		TaskieParser.parse("reflection 2 -deadline 2359 tmr");
+		TaskieParser.parse("reflection 2 due 23.59 tmr");
+		TaskieParser.parse("reflection 2 deadline 23h59 tmr");
+		TaskieParser.parse("reflection 2 -deadline 23:59 tmr");
 		TaskieParser.parse("buy pizza 7pm today");
-		TaskieParser.parse("lab tmr 2 to 3"); // should I recognize this as time?
+		TaskieParser.parse("lab oct 15 from 14h30 to 16"); // should I recognize this as time?
 		TaskieParser.parse("class from 2 to 3");
 		TaskieParser.parse("conference 7 today till 9pm tomorrow");
 		TaskieParser.parse("conference event 7 today till 9pm tmr");
