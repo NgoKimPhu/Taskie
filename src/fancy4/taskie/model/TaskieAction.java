15be016e1d42bb5fb7225e8fbb78f7dfa67b8f9f
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-10-09 21:09:11 +0800

    New attribute to aid in selecting task

--- src/fancy4/taskie/model/TaskieAction.java
+++ src/fancy4/taskie/model/TaskieAction.java
@@ -6,6 +6,8 @@ package fancy4.taskie.model;
 public class TaskieAction {
 	
 	private TaskieEnum.Actions type;
+	private TaskieEnum.TaskType taskType; // used to select Task among the 3 display windows
+											// null = main, deadline = deadline+event, float = float
 	private TaskieTask task;
 	private	int index;
 	private Object keyword;
@@ -15,9 +17,14 @@ public class TaskieAction {
 		this.task = task;
 	}
 	
-	public TaskieAction(TaskieEnum.Actions type, TaskieTask task, int index) {
+	public TaskieAction(TaskieEnum.Actions type, int index, TaskieTask task) {
 		this(type, task);
 		this.index = index;
+	}
+	
+	public TaskieAction(TaskieEnum.Actions type, TaskieEnum.TaskType taskType, int index, TaskieTask task) {
+		this(type, index, task);
+		this.taskType = taskType;
 	}
 	
 	public TaskieAction(TaskieEnum.Actions type, TaskieTask task, Object obj) {
