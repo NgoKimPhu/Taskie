44742cdd9d7719ae3561605c9ca61d88b807ef42
LookRain <brantscube@hotmail.com>
2015-10-21 11:40:14 +0800

    wrote a utility class and its unit tests
    pretty much useless

---
+++ src/fancy4/taskie/model/ResponseProcessor.java
@@ -0,0 +1,9 @@
+package fancy4.taskie.model;
+
+public class ResponseProcessor {
+	public static String process(String[][] input) {
+		String response = input[0][0];
+		return response + "!";
+	}
+	
+}
