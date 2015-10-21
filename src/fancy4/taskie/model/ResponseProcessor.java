3d4d472aee598ecb4cc86ed0fd27de58036164eb
LookRain <brantscube@hotmail.com>
2015-10-21 12:33:15 +0800

    deleted unused test class

--- src/fancy4/taskie/model/ResponseProcessor.java
+++
@@ -1,9 +0,0 @@
-package fancy4.taskie.model;
-
-public class ResponseProcessor {
-	public static String process(String[][] input) {
-		String response = input[0][0];
-		return response + "!";
-	}
-	
-}
