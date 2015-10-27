e55c8ed281a9e2900fb6bf368f00b9e51a77c52c
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-10-27 19:34:34 +0800

    Parse command EXIT

--- src/fancy4/taskie/model/TaskieEnum.java
+++ src/fancy4/taskie/model/TaskieEnum.java
@@ -6,7 +6,7 @@ package fancy4.taskie.model;
 public class TaskieEnum {
 	
 	public static enum Actions {
-		ADD, DELETE, DELETEALL, UNDO, REDO, CLEAR, SEARCH, SORT, UPDATE, DISPLAYALL, RESET, INVALID
+		ADD, DELETE, DELETEALL, UNDO, REDO, CLEAR, SEARCH, SORT, UPDATE, DISPLAYALL, RESET, EXIT, INVALID
 	}
 	
 	public static enum TaskType {
