f15f430f6346edcfcc48b688f7e22728eef21ea9
Qin Xueying <qinxueying1995@gmail.com>
2015-11-01 18:26:24 +0800

    change test case

--- src/fancy4/taskie/model/TaskieStorageTest.java
+++ src/fancy4/taskie/model/TaskieStorageTest.java
@@ -14,10 +14,10 @@ public class TaskieStorageTest {
 		TaskieStorage.deleteAll();
 		TaskieTask float1 = new TaskieTask("finish tutorial");
 		TaskieTask float2 = new TaskieTask("go to dinner with misaki");
-		Date deadline = getDate(2015, 10, 6);
+		Calendar deadline = getDate(2015, 11, 1);
 		TaskieTask deadline1 = new TaskieTask("finish assignment", deadline);
-		Date start = getDate(2015, 10, 5, 14, 0);
-		Date end = getDate(2015, 10, 5, 15, 0);
+		Calendar start = getDate(2015, 10, 5, 14, 0);
+		Calendar end = getDate(2015, 10, 5, 15, 0);
 		TaskieTask event1 = new TaskieTask("meeting", start, end);
 		TaskieStorage.addTask(float1);
 		TaskieStorage.addTask(float2);
@@ -25,39 +25,27 @@ public class TaskieStorageTest {
 		TaskieStorage.addTask(event1);
 		ArrayList<String> keyWords = new ArrayList<String>();
 		keyWords.add("");
-		ArrayList<IndexTaskPair> fs = TaskieStorage.searchTask(keyWords, TaskieEnum.TaskType.FLOAT);
-		ArrayList<IndexTaskPair> ds =  TaskieStorage.searchTask(keyWords, TaskieEnum.TaskType.DEADLINE);
-		ArrayList<IndexTaskPair> es = TaskieStorage.searchTask(keyWords, TaskieEnum.TaskType.EVENT);
-		System.out.println("float:");
+		ArrayList<IndexTaskPair> fs = TaskieStorage.searchTask(keyWords);
+		System.out.println("all:");
 		for(IndexTaskPair f: fs){
 			System.out.println(f.getTask().toString());
-		}
-		System.out.println("event");
-		for(IndexTaskPair e: es){
-			System.out.println(e.getTask().toString());
-		}
-		System.out.println("deadline");
-		for(IndexTaskPair d: ds){
-			System.out.println(d.getTask().toString());
 		}
 		
 		//TaskieStorage.deleteTask(2, TaskieEnum.TaskType.FLOAT);
 		//TaskieStorage.deleteTask(2, TaskieEnum.TaskType.EVENT);
 
 	}
-	public static Date getDate(int year, int month, int day){
+	public static Calendar getDate(int year, int month, int day){
 		Calendar cal = Calendar.getInstance();
 		cal.clear();
 		cal.set(year, month-1, day);
-		Date date = cal.getTime();
-		return date;
+		return cal;
 	}
-	public static Date getDate(int year, int month, int day, int hour, int min){
+	public static Calendar getDate(int year, int month, int day, int hour, int min){
 		Calendar cal = Calendar.getInstance();
 		cal.clear();
 		cal.set(year, month-1, day, hour, min);
-		Date date = cal.getTime();
-		return date;
+		return cal;
 	}
 
 }
