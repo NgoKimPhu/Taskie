e2760bcb69245cdf85ad931f5af6b04e82ec851f
Qin Xueying <qinxueying1995@gmail.com>
2015-11-09 22:52:03 +0800

    abstract calendarpair

--- src/fancy4/taskie/model/TaskieStorage.java
+++ src/fancy4/taskie/model/TaskieStorage.java
@@ -992,35 +992,3 @@ class FileHandler {
 	}
 }
 
-class CalendarPair {
-	private Calendar start;
-	private Calendar end;
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
-
-	public CalendarPair(Calendar start, Calendar end) {
-		this.start = (Calendar) start.clone();
-		this.end = (Calendar) end.clone();
-	}
-
-	public Calendar getStart() {
-		return this.start;
-	}
-
-	public Calendar getEnd() {
-		return this.end;
-	}
-
-	public void setStart(Calendar start) {
-		this.start = start;
-	}
-
-	public void setEnd(Calendar end) {
-		this.end = end;
-	}
-
-	public String toString() {
-		String pair;
-		pair = "(" + sdf.format(this.start.getTime()) + " - " + sdf.format(this.end.getTime()) + ")";
-		return pair;
-	}
-}
