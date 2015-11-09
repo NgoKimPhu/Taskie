8b50c22754041e550ca0dfc9348e5134be3acfdc
Qin Xueying <qinxueying1995@gmail.com>
2015-11-09 14:14:41 +0800

    update unit test

---
+++ src/fancy4/taskie/test/TaskieTaskUnitTest.java
@@ -0,0 +1,14 @@
+package fancy4.taskie.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import fancy4.taskie.model.*;
+
+public class TaskieTaskUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
