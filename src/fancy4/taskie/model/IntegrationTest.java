ff301ece86f0f4ba9fe7aeeabdab1400538db184
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2015-11-09 14:53:09 +0800

    Revert deleting file in commit 5516c5d

---
+++ src/fancy4/taskie/model/IntegrationTest.java
@@ -0,0 +1,78 @@
+package fancy4.taskie.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddFloat() throws UnrecognisedCommandException {
+		TaskieLogic.logic().execute("reset");
+		try {
+			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		TaskieLogic.logic().execute("finish tutorial");
+		assertEquals(TaskieStorage.displayFloatTask().size(), 1);
+	}
+	
+	@Test
+	public void testAddFloatAgain() throws UnrecognisedCommandException {
+		try {
+			TaskieStorage.load("unit/");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		TaskieLogic.logic().execute("finish ps");
+		assertEquals(TaskieStorage.displayFloatTask().size(), 2);
+	}
+	
+	@Test
+	public void testAddDeadline() throws UnrecognisedCommandException {
+		try {
+			TaskieStorage.load("unit/");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		TaskieLogic.logic().execute("finish hw by tmr 9pm");
+		assertEquals(TaskieStorage.displayEventDeadline().size(), 1);
+	}
+	
+	@Test
+	public void testDelete() throws UnrecognisedCommandException {
+		try {
+			TaskieStorage.load("unit/");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		TaskieLogic.logic().execute("del 1");
+		assertEquals(TaskieStorage.displayFloatTask().size(), 1);
+	}
+	
+	@Test
+	// This is the boundary case search empty string, should display all result
+	public void testSearchFloat() throws UnrecognisedCommandException {
+		try {
+			TaskieStorage.load("unit/");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		LogicOutput out = TaskieLogic.logic().execute("view");
+		assertEquals(0, out.getMain().size());
+		assertEquals(0, out.getAll().size());
+	}
+
+}
