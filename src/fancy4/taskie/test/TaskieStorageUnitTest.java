6eefd5285dfddd658c3be8cbc5a419134843a962
Qin Xueying <qinxueying1995@gmail.com>
2015-11-09 19:59:52 +0800

    add test cases

--- src/fancy4/taskie/test/TaskieStorageUnitTest.java
+++ src/fancy4/taskie/test/TaskieStorageUnitTest.java
@@ -70,6 +70,7 @@ public class TaskieStorageUnitTest {
 	public void testValidDelete() {
 		try {
 			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -84,6 +85,7 @@ public class TaskieStorageUnitTest {
 	public void testInvalidDelete() {
 		try {
 			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -104,19 +106,60 @@ public class TaskieStorageUnitTest {
 	public void testSearchWithoutCaseConstrain() {
 		try {
 			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		TaskieTask float1 = new TaskieTask("finish tutorial");
+		TaskieTask float2 = new TaskieTask("Finish assignment");
+		TaskieTask float3 = new TaskieTask("FINISH EXERCISE");
 		TaskieStorage.addTask(float1);
-		try{
-			TaskieStorage.deleteTask(1);
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-			assertEquals("Ooops! index out of the bonds!", e.getMessage());
-		}
-		//not deleted
-		assertEquals(TaskieStorage.displayAllTasks().size(), 1);
+		TaskieStorage.addTask(float2);
+		TaskieStorage.addTask(float3);
+		ArrayList<String> keyWords = new ArrayList<String>();
+		keyWords.add("finish");
+		assertEquals(TaskieStorage.searchTask(keyWords).size(), 3);
 	}
+	
+	@Test 
+	// boundary case, search at a specific time, ie., start == end, event.end == searchKey
+	// both should be found
+	public void testSearchStartEqualsEndDeadlineAndEvent() throws Exception {
+		try {
+			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		Calendar keyDate = Calendar.getInstance();
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		start.set(Calendar.DATE, start.get(Calendar.DATE)-1);
+		TaskieTask deadline = new TaskieTask("finish tutorial", end);
+		TaskieTask event = new TaskieTask("Finish assignment", start, end);
+		TaskieStorage.addTask(deadline);
+		TaskieStorage.addTask(event);
+		assertEquals(TaskieStorage.searchTask(keyDate, keyDate).size(), 2);
+	}
+	
+	@Test 
+	// boundary case, search at a specific time, ie., start == end, event.start = searchKey
+	//should not be found
+	public void testSearchStartEqualsEndEvent() throws Exception {
+		try {
+			TaskieStorage.load("unit/");
+			TaskieStorage.deleteAll();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		Calendar keyDate = Calendar.getInstance();
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		end.set(Calendar.DATE, end.get(Calendar.DATE)+1);
+		TaskieTask event = new TaskieTask("Finish assignment", start, end);
+		TaskieStorage.addTask(event);
+		assertEquals(TaskieStorage.searchTask(keyDate, keyDate).size(), 0);
+	}
+	
 
 }
