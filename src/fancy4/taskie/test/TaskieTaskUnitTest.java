a35f532350c410ee70f80977cbabb2e2662de514
Qin Xueying <qinxueying1995@gmail.com>
2015-11-09 16:03:28 +0800

    modify unit test

--- src/fancy4/taskie/test/TaskieTaskUnitTest.java
+++ src/fancy4/taskie/test/TaskieTaskUnitTest.java
@@ -1,3 +1,4 @@
+//@@author A0119390E
 package fancy4.taskie.test;
 
 import static org.junit.Assert.*;
@@ -8,10 +9,11 @@ import java.util.*;
 public class TaskieTaskUnitTest {
 
 	@Test
-	//boundary case, start time after end time, raise exception
-	public void testCreateTaskStartAfterEnd() {
+	// failure case, start time after end time
+	public void testCreateTaskStartAfterEndFail() {
 		Calendar start = Calendar.getInstance();
 		Calendar end = Calendar.getInstance();
+		//start time one day after end time
 		start.set(Calendar.DATE, start.get(Calendar.DATE)+1);
 		try{
 			TaskieTask task = new TaskieTask("test", start, end);
@@ -20,8 +22,9 @@ public class TaskieTaskUnitTest {
 			System.out.println(e.getMessage());
 		}
 	}
+	
 	@Test
-	//boundary case, task type not match
+	// failure case, task type not match
 	public void testCreateTaskTypeNotMatch() {
 		try{
 			TaskieTask task = new TaskieTask("test", TaskieEnum.TaskType.EVENT, TaskieEnum.TaskPriority.LOW, false, null);
@@ -29,6 +32,62 @@ public class TaskieTaskUnitTest {
 			assertEquals("Task type not match.", e.getMessage());
 			System.out.println(e.getMessage());
 		}
+	}
+	
+	@Test
+	// failure case, start time after end time
+	public void testSetDateStartAfterEndFail() {
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		// start time one day after end time
+		start.set(Calendar.DATE, start.get(Calendar.DATE)+1);
+		try{
+			TaskieTask task = new TaskieTask("test");
+			task.setToEvent(start, end);
+		}catch(Exception e){
+			assertEquals("start time should not after end time.", e.getMessage());
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	// failure case, set start time to deadline task fail
+	public void testSetStartTimeToDeadlineFail() {
+		Calendar start = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		// start time one day before end time
+		start.set(Calendar.DATE, start.get(Calendar.DATE)-1);
+		TaskieTask task = new TaskieTask("test", end);
+		assertFalse(task.setStartTime(start));
+	}
+	
+	@Test
+	// failure case, set start time to floating task fail
+	public void testSetStartTimeToFloatFail() {
+		Calendar start = Calendar.getInstance();
+		// start time one day before current time
+		start.set(Calendar.DATE, start.get(Calendar.DATE)-1);
+		TaskieTask task = new TaskieTask("test");
+		assertFalse(task.setStartTime(start));
+	}
+	
+	@Test
+	// failure case, set end time to floating task fail
+	public void testSetEndTimeToFloatFail() {
+		Calendar end = Calendar.getInstance();
+		TaskieTask task = new TaskieTask("test");
+		assertFalse(task.setStartTime(end));
+	}
+	
+	@Test 
+	// test setting the status to a task
+	public void testSetStatusToTask(){
+		TaskieTask task = new TaskieTask("test");
+		// by default, status is false - incomplete
+		assertTrue(task.setStatus(true));
+		assertFalse(task.setStatus(true));
+		
+		
 	}
 
 }
